c2a2eb75ead881094d758619e02c638ded77d264
Christian Neukirchen <chneukirchen@gmail.com>
2008-02-18 19:49:35 +0100

    Add dependency and versioning handling to hook-get

--- hook-get.rb
+++ hook-get.rb
@@ -1,6 +1,10 @@
 #!/usr/bin/env ruby -w
 
 class HookGet
+  def self.run(file)
+    new.run(file)
+  end
+
   def initialize
     @hooked_in = false
   end
@@ -63,8 +67,58 @@ class HookGet
     sh "hookin", "add", name, *paths
     @hooked_in = true
   end
+
+
+  def depend(expr)
+    installed = `hookin installed`.split
+    package = expr[/^([\w-]*)(-[^-]*)?$/, 1]
+    versions = installed.grep(/^#{package}(-[^-]*)?$/)
+
+    chosen = choose_version(expr, versions)
+    if chosen
+      puts "dependency #{expr} satisfied: found #{chosen}"
+    else
+      available = Dir["#{package}*.rport"].
+                  map { |f| File.basename f, ".rport" }.
+                  grep(/^#{package}(-[^-]*)?$/)
+      to_install = choose_version(expr, available)
+      if to_install
+        puts "hook-get #{to_install}  # satisfies #{expr}"
+        HookGet.run "#{to_install}.rport"
+      else
+        abort "version conflict: unable to satisfy #{expr}"
+      end
+    end
+  end
+
+  def version2array(version)
+    if version =~ /\d+(\.\d+)*$/
+      $&.split('.').map { |f| f.to_i }
+    else
+      [1.0/0]                   # Infinity
+    end
+  end
+  
+  def choose_version(expr, available)
+    min, max = expr.split("...", 2)
+    max ||= min
+    min = version2array min
+    max = version2array max
+    max[-1] += 1
+
+    # just "pkg" means "any version"
+    min = [-1.0/0]  if min == [1.0/0] && max == [1.0/0]
+
+    available.select { |v| (min...max).include? version2array(v) }.
+              sort_by { |v| version2array(v) }.
+              last
+  end
 end
 
 ARGV.each { |file|
-  HookGet.new.run(file)
+  if File.file? file            # the sound of prisoners at night
+    HookGet.run(file)
+  else
+    HookGet.new.depend(file)
+  end
 }
